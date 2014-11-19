9c23c73ad74767a0f3a37f1b1ffe821e6cdd485d
juliataitz <juliataitz@gmail.com>
2014-11-18 20:55:25 -0500

    Swipe and Delete function

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tblTasks: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,19 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        tblTasks.reloadData();
+    }
+    //UITableViewDelegateFunctions
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!){
+        
+        if(editingStyle == UITableViewCellEditingStyle.Delete){
+            taskMgr.tasks.removeAtIndex(indexPath.row)
+            tblTasks.reloadData();
+        }
     }
     
     //UITableViewDataSource
